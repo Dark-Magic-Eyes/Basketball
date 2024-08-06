c0415303d568f597268c956aec607ab5841256b7
VLong <long.tv@atomsolution.com.vn>
2024-08-06 15:11:38 +0700

    Try fix responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export default function Home() {
-  const { unityProvider, sendMessage, loadingProgression } = useUnityContext({
+  const { unityProvider, sendMessage, loadingProgression, isLoaded } = useUnityContext({
     loaderUrl: "Build/Basketball.loader.js",
     dataUrl: "Build/Basketball.data",
     frameworkUrl: "Build/Basketball.framework.js",
@@ -13,9 +13,9 @@ export default function Home() {
     sendMessage("AssetManager", "LoadAssetPreview");
   };
   return (
-    <main className="flex min-h-screen justify-center items-center mx-6 gap-10">
-      <div className="flex items-center justify-center w-[430px] h-[100vh]">
-        <p className="absolute z-[-1]">Loading Application... {Math.round(loadingProgression * 100)}%</p>
+    <main className="flex min-h-screen justify-center items-center gap-10">
+      <div className="flex mx-auto items-center justify-center max-w-[430px] h-[100vh]">
+        {!isLoaded && <p className="absolute z-[-1]">Loading Application... {Math.round(loadingProgression * 100)}%</p>}
         <Unity
           unityProvider={unityProvider}
           style={{ maxWidth: 430, height: "inherit" }}
